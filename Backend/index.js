cde4bad94cb19a3bc0fd8b950097014d32cbccf7
Pritam <90535143+PritamMaharjan86@users.noreply.github.com>
2024-09-25 14:57:10 +1000

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors');
-const PORT = process.env.PORT || 3001;
+const PORT = https://signup-backend-five.vercel.app/;
 const AuthRouter = require('./Routes/AuthRouter')
 
 require('dotenv').config();
