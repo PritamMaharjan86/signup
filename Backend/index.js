87c5f24bbb57210ff00e3dc889516d72f05beb5c
Pritam Maharjan <primgdev@gmail.com>
2024-10-02 15:10:44 +1000

    update

--- Backend/index.js
+++ Backend/index.js
@@ -9,20 +9,13 @@ const AuthRouter = require('./Routes/AuthRouter')
 require('dotenv').config();
 require('./Models/Database')
 
-const corsOptions = {
-    origin: 'https://signup-beryl-eta.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Add Content-Type and other headers if needed
-};
 
-app.get('/server', (req, res) =>
-    res.send('Express on vercel'))
+app.get('/server', (req, res) => {
+    res.send('Server is running now...');
+})
 
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));  // Handle preflight requests
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors())
 app.use('/auth', AuthRouter);
 
 app.listen(PORT || process.env.PORT, () => {
