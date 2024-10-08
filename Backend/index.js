b4a80132626fbdf746a3dff4e91552c58993fc4f
Pritam Maharjan <primgdev@gmail.com>
2024-10-08 20:22:36 +1100

    update

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const PORT = 3001;
 const AuthRouter = require('./Routes/AuthRouter')
+const corsConfig = {
+    origin:'*',
+    credential: true,
+    methods:["GET","POST","PUT","DELETE"],
+}
 
 require('dotenv').config();
 require('./Models/Database')
@@ -17,6 +22,7 @@ app.get('/server', (req, res) => {
 app.use(bodyParser.json());
 app.use(cors())
 app.use('/auth', AuthRouter);
+app.options("", cors(corsConfig))
 
 app.listen(PORT || process.env.PORT, () => {
     console.log(`Server is running on ${PORT}`);
