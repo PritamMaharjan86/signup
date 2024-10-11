a511264755dc75b2dc0b406a71a9cabf274dc3f5
Pritam Maharjan <primgdev@gmail.com>
2024-10-11 19:13:51 +1100

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
+
 const cors = require('cors');
 const PORT = 3001;
 const AuthRouter = require('./Routes/AuthRouter');
