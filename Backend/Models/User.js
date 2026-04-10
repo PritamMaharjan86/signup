b9fc3dc76bb75ecb3a9d5daee39408cdc0c30f81
Pritam Maharjan <primgdev@gmail.com>
2026-04-10 16:10:24 +1000

    updatE

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -1,24 +1,16 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   isVerified: { type: Boolean, default: false },
   verifyToken: { type: String },
   verifyExpiry: { type: Date },
 });
 
 const UserModel = mongoose.model("users", UserSchema);
-module.exports = UserModel;
+
+export default UserModel;
